25e8478ed2af42a46bb06f6e476c24fc07f50b2d
Abdo Mahfouz <abdomohmmedmahfouz@gmail.com>
2026-04-19 14:38:53 +0200

    Remove oldDashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,11 +18,11 @@ const routes: Routes = [
   },
 
   // Unified dashboard feature module
-  {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
+  // {
+  //   path: 'dashboard',
+  //   loadChildren: () =>
+  //     import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
+  // },
 
   // Admin area
   {
